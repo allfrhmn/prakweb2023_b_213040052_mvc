d47d1010cd5a389bfaefb692139cd5dc87acb04a
allfrhmn <allfiandirhmn@gmail.com>
2023-10-20 14:13:21 +0700

    #7 Model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,7 @@
 class Home extends Controller {
     public function index() {
         $data['judul'] = 'Home';
-        $data['nama'] = 'Allfiandi Rahman';
+        $data['nama'] = $this->model('User_model')->getUser();
         $this->view('templates/header', $data);
         $this->view('home/index', $data);
         $this->view('templates/footer');
